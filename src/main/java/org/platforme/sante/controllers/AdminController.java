d12fbf4cf8e14d6a29f60c84bc565229e97b8e87
mounir89 <mounirhamdane@yahoo.fr>
2016-08-11 18:46:36 +0200

    ajouter controller

---
+++ src/main/java/org/platforme/sante/controllers/AdminController.java
@@ -0,0 +1,20 @@
+package org.platforme.sante.controllers;
+
+import org.platforme.sante.metier.IAdminSanteMetier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@Autowired
+	private IAdminSanteMetier metier;
+	
+	
+	
+	
+
+}
